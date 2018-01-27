e845072d656958fc23d365f0ecbe6dbe62646334
raghavendra.udyavar <raghavs.mail@gmail.com>
2018-01-27 22:46:50 +0800

    refactor the use of promises and introduce async await

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,15 +3,14 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+beforeEach(async () => {
     // get list of all accounts
-    web3.eth.getAccounts().
-    then(
-        fetchedAccounts => {
-            console.log(fetchedAccounts);
-    });
+    accounts = await (web3.eth.getAccounts());
 });
 
 describe('Inbox', () => {
-    it('deploys a contract',() => {});
+    it('deploys a contract',() => {
+       console.log(accounts);
+    });
 });
